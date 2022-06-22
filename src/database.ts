1d7a631e5351a94634c9c84ab280bd8873183526
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-06-22 12:41:01 +0200

    eslint, prettier, ts setup

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,14 @@
-import dotenv from "dotenv";
-import { Pool } from "pg";
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
-dotenv.config();
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+dotenv.config()
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env
 
 const client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
+	host: POSTGRES_HOST,
+	database: POSTGRES_DB,
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD,
+})
 
-export default client;
+export default client
